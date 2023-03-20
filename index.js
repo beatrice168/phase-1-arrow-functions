c0f2bf7336fd6dc2007979b750199bb66d3f422d
beatrice <helgapaul389@gmail.com>
2023-03-20 08:41:08 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+function divide(_num1, _num2) {
+    return 2000 / 100;
+  }
+  const square = (_num) => {
+    return 2 * 2;
+  };
+  const add = (num1, num2) => {
+    return num1 + num2;
+  };
+    
